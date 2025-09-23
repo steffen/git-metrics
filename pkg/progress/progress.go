06fea8f32a084934b9eaa2bfd58e8b3718e33d20
Steffen Hiller <6301+steffen@users.noreply.github.com>
2025-09-23 15:34:11 -0700

    Fix formatting in progress display: add space between year and spinner for improved readability.

--- pkg/progress/progress.go
+++ pkg/progress/progress.go
@@ -57,7 +57,7 @@ func UpdateProgress() {
 		return
 	}
 	fmt.Printf("\r%-6s%10s %8s %5s %7s │%12s %10s %5s %7s │%13s %12s %5s %7s",
-		fmt.Sprintf("%d%s", CurrentProgress.Year, ProgressSpinner.Next()),
+		fmt.Sprintf("%d %s", CurrentProgress.Year, ProgressSpinner.Next()),
 		utils.FormatNumber(CurrentProgress.Statistics.Authors),
 		"...",
 		"...",
